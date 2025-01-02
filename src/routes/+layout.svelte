1b9919f81d7374c2f16a41a725f81d0dbc42ddfc
Core <daverencordero@gmail.com>
2025-01-02 23:23:39 +0800

    feat: added a simple nav layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
+	import { page } from '$app/state';
 	import '../app.css';
+	import { NavBrand, NavHamburger, NavUl, NavLi, Navbar } from 'flowbite-svelte';
+
+	const activeUrl = page.url.pathname;
 </script>
 
-<slot></slot>
+<Navbar rounded color="form">
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-bold text-primary-600">
+			Conflicted Artist
+		</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl}>
+		<NavLi data-sveltekit-reload href="/">Home</NavLi>
+		<NavLi href="/about">Blog</NavLi>
+		<NavLi href="/photography">Photography</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
+
+<div>
+	<slot></slot>
+</div>
