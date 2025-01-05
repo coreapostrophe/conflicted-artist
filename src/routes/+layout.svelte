b19216fe2e3ad34a057114ccec2bfd756d6e4ce1
Core <daverencordero@gmail.com>
2025-01-05 13:57:20 +0800

    feat: added simple footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,23 +2,34 @@
 	import '../app.css';
 	import '@fontsource-variable/work-sans';
 	import { page } from '$app/state';
-	import { NavBrand, NavHamburger, NavUl, NavLi, Navbar } from 'flowbite-svelte';
+	import {
+		NavBrand,
+		NavHamburger,
+		NavUl,
+		NavLi,
+		Navbar,
+		Footer,
+		FooterLinkGroup,
+		FooterLink,
+		FooterCopyright
+	} from 'flowbite-svelte';
+	import { base } from '$app/paths';
 
 	const { children } = $props();
 </script>
 
 {#snippet navLink(title: string, link: string)}
-	<NavLi class="cursor-pointer" href={link}>{title}</NavLi>
+	<NavLi href="{base}{link}">{title}</NavLi>
 {/snippet}
 
-<Navbar rounded color="form">
-	<NavBrand href="/">
-		<span class="self-center whitespace-nowrap text-2xl font-extrabold text-primary-700">
+<Navbar color="form">
+	<NavBrand href="{base}/">
+		<span class="self-center whitespace-nowrap text-xl font-extrabold text-primary-700 md:text-2xl">
 			Conflicted Artist
 		</span>
 	</NavBrand>
 	<NavHamburger />
-	<NavUl activeUrl={page.url.pathname}>
+	<NavUl class="mr-3" activeUrl={page.url.pathname}>
 		{@render navLink('Home', '/')}
 		{@render navLink('Blog', '/blog')}
 		<!-- <NavLi href="/photography">Photography</NavLi> -->
@@ -28,9 +39,23 @@
 	</NavUl>
 </Navbar>
 
-<div class="container mx-auto mt-4">
+<div class="container mx-auto mt-4 p-2 md:p-0">
 	{@render children()}
 </div>
+
+<Footer
+	class="bg-grey-50 absolute bottom-0 start-0 z-20 w-full p-4 text-center md:flex md:items-center md:justify-between md:p-6"
+>
+	<FooterCopyright href="/" by="Daveren Cordero" year={2025} />
+	<FooterLinkGroup
+		ulClass="flex flex-wrap gap-x-4 gap-y-1 justify-center items-center mt-3 text-sm text-gray-500 dark:text-gray-400 md:mt-0"
+	>
+		<FooterLink liClass="m-0" href="/">About</FooterLink>
+		<FooterLink liClass="m-0" href="/">Privacy Policy</FooterLink>
+		<FooterLink liClass="m-0" href="/">Licensing</FooterLink>
+		<FooterLink liClass="m-0" href="/">Contact</FooterLink>
+	</FooterLinkGroup>
+</Footer>
 
 <style>
 	:global(body) {
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,13 @@
-<p>Blog page works!</p>
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+
+	const { data } = $props();
+	const { blogPosts } = data;
+</script>
+
+{#snippet postItem()}
+	<Card>test</Card>
+{/snippet}
+
+<h1 class="text-3xl">Posts</h1>
+{@render postItem()}
